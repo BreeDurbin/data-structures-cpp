28c48c2ee48ce4ed9ac5102429a6e2c6dc3c98d0
saya <sayamustfight@proton.me>
2025-08-29 03:47:04 -0500

    quiz 1.5 solution

---
+++ learn_cpp/CH_1/1.5_b.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Enter 3 numbers separated by spaces: " ;
+    int x{}, y{}, z{};
+
+    cin >> x >> y >> z;
+
+    cout << "You entered " << x << ", " << y << ", and " << z << ".\n";
+
+    return 0;
+}
